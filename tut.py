adb4e60a7ae0c42f5b5f4eefaa616a2a9f1d8ffb
Simon Myunggun Seo <ms9144@nyu.edu>
2023-04-07 14:04:45 -0400

    incorporate both modules into pipeline

--- tut.py
+++ tut.py
@@ -1,15 +1,17 @@
 from story_builder import StoryBuilder
+from illustrator.stable_diffusion import StableDiffusionIllustrator
 import yaml
 
 
 def create_illustrator(config):
     # instantiate type of illustrator from yaml config
     print("config:", config)
-    pass
-
+    illustrator = StableDiffusionIllustrator(**config)
+    return illustrator
 
 def create_story_builder(config):
-    pass
+    story_builder = StoryBuilder(**config)
+    return story_builder
 
 
 class TellUrTalePipeline(object):
@@ -19,15 +21,28 @@ class TellUrTalePipeline(object):
         self.illustrator = create_illustrator(config["ilustrator"])
 
     def run_tut(self):
-        # build story
-        # call illustrator
-        pass
+        # story_name, story_prompts = self.story_builder.interactive_build()
+        story_name = "test_story"
+        story_prompts = [
+            "a momma bear walked up to a monkey.",
+            "the momma bear and monkey skied down a hill.",
+            "the momma bear and monkey fell down.",
+        ]
+        story_images = self.illustrator.generate(story_prompts)
+        # return self.illustrator.write_illustration(story_images)
+        return story_images
+
 
 
 if __name__ == '__main__':
     with open('config.yml', 'r') as config_file:
-        config = yaml.safe_load(config_file)
+        config: dict = yaml.safe_load(config_file)
     print(f"Running {config['project_name']}...")
 
     pipeline = TellUrTalePipeline(config)
+
+    # uncomment when storybuilder becomes interactive
+    # while True:
+    #     pipeline.run_tut()
+    
     pipeline.run_tut()
